fbdb6ede30a4ba6453a5c23f2ae0173e7c73fc61
nhamlani101 <nh1083@nyu.edu>
2015-06-26 13:01:10 -0400

    extras

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
+  $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+        
