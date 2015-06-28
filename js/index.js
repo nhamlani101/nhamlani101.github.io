698fb2ddef2a9cc4c72d70cf948604451b3b3cbb
nhamlani101 <nh1083@nyu.edu>
2015-06-27 20:03:53 -0400

    massive overhaul

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
- $(document).ready(function(){
-    $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
+$(document).ready(function(){
+	$('.collapsible').collapsible({
+	accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
-  $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-        
+});
+
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
+
+  
